1a637fd2bc3a537eb3dd4597471344440fa9fdd6
Billy Gao <billy.gao@hotmail.com>
2024-02-17 11:48:13 -0800

    Homescreen!

--- repo/src/provider/View/ProviderHomeScreen.js
+++ repo/src/provider/View/ProviderHomeScreen.js
@@ -4,8 +4,8 @@ import React from 'react';
 function ProviderHomeScreen({ profile }) {
   return (
     <div className="provider-home">
-      <h1>Patient Home</h1>
-      <p>Welcome back, {profile.name}!</p>
+      <h1>Provider Home</h1>
+      <p>Welcome back!</p>
       {/* Additional content for the provider home screen can be added here */}
     </div>
   );
