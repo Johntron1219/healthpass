0a595e008df4fd4bd0c1181f304489068dccc5fe
SMandadi9 <smandadi@berkeley.edu>
2024-02-17 01:59:16 -0800

    create patient profile backend

--- repo/src/patient/Backend/createPatient.js
+++ repo/src/patient/Backend/createPatient.js
@@ -3,13 +3,32 @@
 - input: first name, last name, DOB, address, email, phone
 - creates document in MongoDB under patient collection 
 */
+import { firestore } from './firebase';
+
+const checkIfDocumentExists = async (collectionName, documentId) => {
+    try {
+      const documentRef = firestore.collection(collectionName).doc(documentId);
+      const documentSnapshot = await documentRef.get();
+      return documentSnapshot.exists;
+    } catch (error) {
+      console.error('Error checking document: ', error);
+      return false;
+    }
+  };
+
+const addDocumentToCollection = async (collectionName, data) => {
+  try {
+    const collectionRef = firestore.collection(collectionName);
+    await collectionRef.add(data);
+    console.log('Document added successfully');
+  } catch (error) {
+    console.error('Error adding document: ', error);
+  }
+};
 
 export const createPatient = (patientData) => {
 
-    // call to see if patient already exists in database
-
-    // if not exists, then create a new record for the patient, return successful login page on creation
-
-
-    // if does exists, return error 'Patient profile already exists'
+    if (checkIfDocumentExists('patients', patientData.id)) {
+        addDocumentToCollection('patients', patientData);
+    }
 }
---
+++ repo/src/patient/Views/createPatientProfile.js
@@ -0,0 +1,1 @@
+/* view to create initial patient profile */
