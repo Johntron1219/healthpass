24924571eec56b1523c9f733c980092d3de7c088
John L. Corso Jr <johncorso@DN0a22a5d7.SUNet>
2024-02-17 21:51:06 -0800

    completed createPatient code

--- repo/src/patient/Backend/createPatient.js
+++ repo/src/patient/Backend/createPatient.js
@@ -28,6 +28,38 @@ const addDocumentToCollection = async (collectionName, data) => {
 
 /* patientData - {email: String, password: String, PID: Number} */
 
+const patient = async (email, password, pid) => {
+  const patientData = {
+    PID: pid,
+    authorizedNPIs: [null],
+    email: email,
+    password: password,
+    metadata: [null],
+    firstname: null,
+    middlename: null,
+    lastname: null,
+    monthofbirth: null,
+    dayofbirth: null,
+    yearofbirth: null,
+    address: null,
+    city: null,
+    state: null,
+    ZIP: null,
+    insurancename: null,
+    insurancenumber: null,
+    phone: null,
+    sex: null,
+    pastNPIs: [{ NPI: null }],
+    medications: [{ NDC: null, dose: null, medNPI: null }],
+    allergies: [{ allergen: null, severity: null, algNPI: null }],
+    conditions: [{ ICD10: null, diagnosisDate: null, dxNPI: null }],
+    procedures: [{ HCPCS: null, date: null, procedureNPI: null }],
+    immunizations: [{ shotname: null, date: null, shotNPI: null }],
+    labrecords: [{ HCPCS: null, value: null, date: null, labNPI: null }],
+  };
+  return patientData;
+};
+
 export const createPatient = (patientData) => {
 
     if (checkIfDocumentExists('patients', patientData.id)) {
