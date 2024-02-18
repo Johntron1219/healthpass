76f6e00c25cef69ad7c38b682c6cb1e2680ea09c
Billy Gao <billy.gao@hotmail.com>
2024-02-18 03:28:41 -0800

    Update ProviderScreen.js

--- repo/src/provider/View/ProviderScreen.js
+++ repo/src/provider/View/ProviderScreen.js
@@ -6,7 +6,6 @@ import PatientDetailScreen from './PatientDetailScreen';
 import PatientEditForm from './PatientEditForm';
 import RequestsScreen from './RequestsScreen';
 import Pic from '../../public/DTS.webp';
-import { getProviderData } from '../Backend/getProviderData'; // Import the function to fetch provider data
 import { getAllPatientData } from '../Backend/getRecords/getPatientData'; // Correct import
 import getConditions from '../Backend/getRecords/getConditions';
 import getAllergies from '../Backend/getRecords/getAllergies';
@@ -14,86 +13,85 @@ import getMedications from '../Backend/getRecords/getMedications';
 import getProcedures from '../Backend/getRecords/getProcedures';
 import getImmunizations from '../Backend/getRecords/getImmunizations';
 import getLabRecords from '../Backend/getRecords/getLabRecords';
+import { getProviderData } from '../Backend/getProviderData';
 import { updatePatientField } from '../../patient/Backend/updatePatientField';
 
 function ProviderScreen({ setCurrentScreen, providerNPI }) {
   const [providerScreen, setProviderScreen] = useState('home');
   const [selectedPatient, setSelectedPatient] = useState(null);
-  const [providerProfile, setProviderProfile] = useState(null); // State to store provider profile
+  const [profile, setProfile] = useState(null);
   const [patientProfiles, setPatientProfiles] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Fetch provider profile
-        const providerData = await getProviderData(providerNPI);
-        setProviderProfile(providerData);
-
-        // Fetch all patients
-        const patientsData = await getAllPatientData();
-        
-        // Convert the object values into an array of patients
-        const patientsArray = Object.values(patientsData);
-        
-        // Fetch additional data for each patient
-        const profiles = await Promise.all(patientsArray.map(async (patient) => {
-          // Check if all required properties exist before accessing them
-          if (
-            patient.metadata &&
-            patient.metadata.firstname &&
-            patient.metadata.middlename &&
-            patient.metadata.lastname &&
-            patient.metadata.address &&
-            patient.metadata.city &&
-            patient.metadata.state &&
-            patient.metadata.zip &&
-            patient.metadata.monthofbirth &&
-            patient.metadata.dayofbirth &&
-            patient.metadata.yearofbirth &&
-            patient.email &&
-            patient.metadata.insurancename &&
-            patient.metadata.insurancenum
-          ) {
-            const conditions = await getConditions(patient.pt);
-            const allergies = await getAllergies(patient.pt);
-            const medications = await getMedications(patient.pt);
-            const procedures = await getProcedures(patient.pt);
-            const immunizations = await getImmunizations(patient.pt);
-            const labRecords = await getLabRecords(patient.pt);
-  
-            return {
-              pt: patient.pt,
-              name: `${patient.metadata.firstname} ${patient.metadata.middlename} ${patient.metadata.lastname}`,
-              lastEditDate: "2024-02-17",
-              address: `${patient.metadata.address}, ${patient.metadata.city}, ${patient.metadata.state} ${patient.metadata.zip}`,
-              dob: `${patient.metadata.monthofbirth}/${patient.metadata.dayofbirth}/${patient.metadata.yearofbirth}`,
-              email: patient.email,
-              insurancePolicyNumber: patient.metadata.insurancename,
-              insurancePlan: patient.metadata.insurancenum,
-              conditions,
-              allergies,
-              medications,
-              procedures,
-              immunizations,
-              labRecords
-            };
-          } else {
-            console.error('Patient data is missing required properties:', patient);
-            return null; // Return null for patients with missing properties
-          }
-        }));
-  
-        // Filter out null profiles (patients with missing properties)
-        const filteredProfiles = profiles.filter(profile => profile !== null);
-  
-        setPatientProfiles(filteredProfiles);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-  
-    fetchData();
+    fetchData(providerNPI);
   }, [providerNPI]);
+
+  const fetchData = async (NPI) => {
+    try {
+      const response = await getProviderData(NPI);
+      const providerProfile = {
+        name: response?.name || "Jose Doe",
+        address: response?.address || "123 Main St, Anytown, AN 12345",
+        photo: Pic,
+        phone: response?.phone || "12345678",
+        email: response?.email || "jose.doe@example.com",
+        NPI: response?.NPI || "1234567890",
+      };
+      setProfile(providerProfile);
+
+      const patientsData = await getAllPatientData();
+      const patientsArray = Object.values(patientsData);
+      const profiles = await Promise.all(patientsArray.map(async (patient) => {
+        if (
+          patient.metadata &&
+          patient.metadata.firstname &&
+          patient.metadata.middlename &&
+          patient.metadata.lastname &&
+          patient.metadata.address &&
+          patient.metadata.city &&
+          patient.metadata.state &&
+          patient.metadata.zip &&
+          patient.metadata.monthofbirth &&
+          patient.metadata.dayofbirth &&
+          patient.metadata.yearofbirth &&
+          patient.email &&
+          patient.metadata.insurancename &&
+          patient.metadata.insurancenum
+        ) {
+          const conditions = await getConditions(patient.pt);
+          const allergies = await getAllergies(patient.pt);
+          const medications = await getMedications(patient.pt);
+          const procedures = await getProcedures(patient.pt);
+          const immunizations = await getImmunizations(patient.pt);
+          const labRecords = await getLabRecords(patient.pt);
+
+          return {
+            pt: patient.pt,
+            name: `${patient.metadata.firstname} ${patient.metadata.middlename} ${patient.metadata.lastname}`,
+            lastEditDate: "2024-02-17",
+            address: `${patient.metadata.address}, ${patient.metadata.city}, ${patient.metadata.state} ${patient.metadata.zip}`,
+            dob: `${patient.metadata.monthofbirth}/${patient.metadata.dayofbirth}/${patient.metadata.yearofbirth}`,
+            email: patient.email,
+            insurancePolicyNumber: patient.metadata.insurancename,
+            insurancePlan: patient.metadata.insurancenum,
+            conditions,
+            allergies,
+            medications,
+            procedures,
+            immunizations,
+            labRecords
+          };
+        } else {
+          console.error('Patient data is missing required properties:', patient);
+          return null;
+        }
+      }));
+      const filteredProfiles = profiles.filter(profile => profile !== null);
+      setPatientProfiles(filteredProfiles);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   const handleSelectPatient = (patient) => {
     setSelectedPatient(patient);
@@ -105,7 +103,6 @@ function ProviderScreen({ setCurrentScreen, providerNPI }) {
   };
 
   const handleSavePatient = async (updatedPatient) => {
-    // Update the local state first
     setPatientProfiles((prevProfiles) => {
       const index = prevProfiles.findIndex(profile => profile.pt === updatedPatient.pt);
       if (index !== -1) {
@@ -116,19 +113,15 @@ function ProviderScreen({ setCurrentScreen, providerNPI }) {
       return prevProfiles;
     });
 
-    // Update the selectedPatient state to reflect the changes immediately
     setSelectedPatient(updatedPatient);
 
-    // Here you should call a function to update the patient in Firebase
     try {
-      // Assuming 'pt' is the patient ID and it is a string. If it's not, you should convert it accordingly.
       await updatePatientField(updatedPatient.pt, updatedPatient);
       console.log('Patient data updated successfully in Firebase');
     } catch (error) {
       console.error('Failed to update patient data in Firebase', error);
     }
 
-    // Go back to the patient detail screen
     setProviderScreen('patientDetail');
   };
 
@@ -144,10 +137,10 @@ function ProviderScreen({ setCurrentScreen, providerNPI }) {
   let screenComponent;
   switch (providerScreen) {
     case 'home':
-      screenComponent = <ProviderHomeScreen profile={providerProfile} />;
+      screenComponent = <ProviderHomeScreen profile={profile} />;
       break;
     case 'profile':
-      screenComponent = <ProviderProfileScreen profile={providerProfile} />;
+      screenComponent = <ProviderProfileScreen profile={profile} />;
       break;
     case 'patientList':
       screenComponent = <PatientListScreen patientProfiles={patientProfiles} onPatientSelect={handleSelectPatient} />;
@@ -162,7 +155,7 @@ function ProviderScreen({ setCurrentScreen, providerNPI }) {
       screenComponent = <RequestsScreen providerNPI={providerNPI} />;
       break;
     default:
-      screenComponent = <ProviderHomeScreen profile={providerProfile} />;
+      screenComponent = <ProviderHomeScreen profile={profile} />;
   }
 
   return (
