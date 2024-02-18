4bad43f41fd77fd1e9b54b6de053f88a80b3ce70
Johntron1219 <99308415+Johntron1219@users.noreply.github.com>
2024-02-17 19:02:56 -0500

    module to fetch data

---
+++ repo/src/patient/Views/fetchptdata.js
@@ -0,0 +1,35 @@
+import { database } from '../../firebase'; // Adjust the path as necessary
+
+const fetchPtData = async (pt, path) => {
+  const docRef = database.collection('patients').doc(pt);
+
+  try {
+    const doc = await docRef.get();
+    if (doc.exists) {
+      const jsonData = doc.data();
+      const value = getValueByPath(jsonData, path);
+      return value;
+    } else {
+      console.log('No such document!');
+      return null;
+    }
+  } catch (error) {
+    console.log('Error getting document:', error);
+    return null;
+  }
+};
+
+const getValueByPath = (object, path) => {
+  const keys = path.split('.');
+  let value = object;
+  keys.forEach((key) => {
+    if (value && key in value) {
+      value = value[key];
+    } else {
+      value = null;
+    }
+  });
+  return value;
+};
+
+export default fetchPtData;
